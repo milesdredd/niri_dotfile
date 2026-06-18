4619306c4969b42aa908cc80fc05967e083e05c5
milesdredd <ankitraj97093@gmail.com>
2026-06-18 13:51:24 +0530

    push
    bar layer blur added

--- struts.sh
+++ struts.sh
@@ -4,7 +4,7 @@ CONFIG="$HOME/.config/niri/struts.kdl"
 
 #niri msg action do-screen-transition
 
-sed -i -E 's/^([[:space:]]*top )0$/\120/; t; s/^([[:space:]]*top )20$/\10/' "$CONFIG"
+sed -i -E 's/^([[:space:]]*top )0$/\118/; t; s/^([[:space:]]*top )18$/\10/' "$CONFIG"
 
 if grep -q '^[[:space:]]*top 0$' "$CONFIG"; then
     eww update bar=false
